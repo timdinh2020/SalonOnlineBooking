7cff50fe5ea39533d56eea740d6f8d503a3efb5b
timdinh2020 <56038061+timdinh2020@users.noreply.github.com>
2022-04-08 03:59:18 -0400

    TD-DatabaseTesting
    created Account.cs as a map for account table in the database.
    Testing code is in Startup.cs

---
+++ TestApp/TestApp/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace TestApp
+{
+    public class Account
+    {
+        //[BsonElement("_id")]
+        public BsonObjectId Id { get; set; }
+
+        //[BsonElement("first_name")]
+        public string first_name { get; set; }
+
+        //[BsonElement("last_name")]
+        public string last_name { get; set; }
+
+        //[BsonElement("role")]
+        public string role { get; set; }
+
+        //[BsonElement("email")]
+        public string email { get; set; }
+
+        //[BsonElement("username")]
+        public string username { get; set; }
+
+        //[BsonElement("password")]
+        public string password { get; set; }
+
+        //[BsonElement("avail_days")]
+        public string avail_days { get; set; }
+
+        public void GetAccount()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:<sob123>@cluster1.8dqvn.mongodb.net/Salon_Online_Booking?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+            var mongodb = dbClient.GetDatabase("Salon_Online_Booking");
+            var accounts = mongodb.GetCollection<BsonDocument>("accounts");
+
+            /*
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+            */
+        }
+    }
+}
--- TestApp/TestApp/Startup.cs
+++ TestApp/TestApp/Startup.cs
@@ -3,6 +3,8 @@ using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 [assembly: OwinStartup(typeof(TestApp.Startup))]
 
@@ -13,6 +15,20 @@ namespace TestApp
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            /**************************
+             * Database testing
+             */
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:sob123@cluster1.8dqvn.mongodb.net/Salon_Online_Booking?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+            var mongodb = dbClient.GetDatabase("Salon_Online_Booking");
+            var accounts = mongodb.GetCollection<Account>("accounts");
+
+            var owner = accounts.Find(a => a.role == "owner").ToList();
+            /**************************
+             * Database testing END
+             */
         }
     }
 }
