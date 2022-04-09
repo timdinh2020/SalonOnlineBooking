fd9330f5b66732e627b58367b438e2502f5840bc
Alexis <62515999+A-Bun@users.noreply.github.com>
2022-04-08 20:23:37 -0400

    Account Class Creation
    Account, hair-dresser, owner, and member classes have been created and partially completed.

---
+++ TestApp/TestApp/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class Owner : Account
+    {
+        public string Role = "owner";
+    }
+}
